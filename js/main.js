e928af418f5758ff5783dcc299ad0365fabdd17d
deysan <andrew.deysan@gmail.com>
2017-10-14 00:31:07 +0300

    Написал скрипт для всплывающего окна

--- js/main.js
+++ js/main.js
@@ -1,19 +1,50 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+// МЕНЮ В МОБИЛЬНОЙ ВЕРСИИ
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
-navToggle.classList.remove('main-nav__toggle--nojs');
+navMain.classList.remove("main-nav--nojs");
+navToggle.classList.remove("main-nav__toggle--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navToggle.classList.remove('main-nav__toggle--closed');
-    navMain.classList.add('main-nav--opened');
-    navToggle.classList.add('main-nav__toggle--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    navToggle.classList.remove("main-nav__toggle--closed");
+    navToggle.classList.add("main-nav__toggle--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navToggle.classList.add('main-nav__toggle--closed');
-    navMain.classList.remove('main-nav--opened');
-    navToggle.classList.remove('main-nav__toggle--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+    navToggle.classList.add("main-nav__toggle--closed");
+    navToggle.classList.remove("main-nav__toggle--opened");
   }
 });
+
+// МОДАЛЬНОЕ ОКНО "ДОБАВИТЬ В КОРЗИНУ"
+var toCart = document.querySelectorAll(".tocart");
+var modaltoCart = document.querySelector(".modal-tocart");
+var overLay = document.querySelector(".modal-overlay");
+
+for (var i = 0; i < toCart.length; i++) {
+  toCart[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    overLay.classList.add("modal-overlay--show");
+    modaltoCart.classList.remove("modal-tocart--closed");
+    modaltoCart.classList.add("modal-tocart--opened");
+  });
+
+  overLay.addEventListener("click", function(event) {
+    overLay.classList.remove("modal-overlay--show");
+    modaltoCart.classList.remove("modal-tocart--opened");
+    modaltoCart.classList.add("modal-tocart--closed");
+  });
+
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+      if (modaltoCart.classList.contains("modal-tocart--opened")) {
+        overLay.classList.remove("modal-overlay--show");
+        modaltoCart.classList.remove("modal-tocart--opened");
+        modaltoCart.classList.add("modal-tocart--closed");
+      }
+    }
+  });
+}
